85a62ca07e2ee6ccbf1da6677fc7c9038e877c36
ikram3031 <mdikram.3031@gmail.com>
2023-12-06 22:47:29 +0600

    lets install payload

---
+++ src/get-payload.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+import path from 'path'
+import type { InitOptions } from 'payload/config'
+import payload, { Payload } from 'payload'
+// import nodemailer from 'nodemailer'
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env')
+})
+
+let cached = (global as any).payload
+
+if (!cached) {
+    cached = (global as any).payload = {
+        client: null,
+        promise: null
+    }
+}
+
+interface Args {
+    initOptions?: Partial<InitOptions>
+}
+
+export const getPayloadClient = async () => { }
