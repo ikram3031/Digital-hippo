083e983b72b24d71d97a9ffb419fb62338d8d868
ikram3031 <mdikram.3031@gmail.com>
2023-12-14 00:05:28 +0600

    46 mins

---
+++ app/{marketing}/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+type Props = {}
+
+const MarketingPage = (props: Props) => {
+    return (
+        <div className='flex items-center justify-center flex-col'>
+            MarketingPage
+        </div>
+    )
+}
+
+export default MarketingPage
