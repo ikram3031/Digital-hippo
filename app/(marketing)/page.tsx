cb7d527d7f0f63ab884eb7f33c8a7b0592274fed
ikram3031 <mdikram.3031@gmail.com>
2023-12-21 22:04:52 +0600

    01:01 h

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,15 +1,39 @@
 'use client'
 
+import localFont from 'next/font/local';
 import { Medal } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-type Props = {}
+import { cn } from '@/lib/utils';
+import { Poppins } from "next/font/google";
+
+const headingFont = localFont({
+    src: "../../public/fonts/font.woff2",
+})
+
+const textFont = Poppins({
+    subsets: ["latin"],
+    weight: [
+        "100",
+        "200",
+        "300",
+        "400",
+        "500",
+        "600",
+        "700",
+        "800",
+        "900"
+    ],
+});
 
 const MarketingPage = (props: Props) => {
     return (
         <div className='flex items-center justify-center flex-col'>
-            <div className="flex items-center justify-center flex-col">
+            <div className={cn(
+                "flex items-center justify-center flex-col",
+                headingFont.className,
+            )}>
                 <div
                     className="mb-4 flex items-center border 
                     shadow-sm p-4 bg-amber-100 text-amber-700
@@ -25,9 +49,10 @@ const MarketingPage = (props: Props) => {
                     work forward.
                 </div>
             </div>
-            <div className=
-                "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto"
-            >
+            <div className={cn(
+                "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+                textFont.className,
+            )}>
                 Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
             </div>
             <Button className="mt-6" size="lg" asChild>
