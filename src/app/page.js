087f0ba1e6fc4adda2b8435e189911a32c9065fa
ikram3031 <mdikram.3031@gmail.com>
2023-12-22 19:31:32 +0600

    27:00

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,40 @@
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
-    <main >
-      <header>
-        <Link href={'/'}>LiskList</Link>
-      </header>
+    <main>
+      <Header />
+
+      <section className="p-6 pt-32 max-w-4xl mx-auto">
+        <div className="max-w-md mb-8">
+          <h1 className="text-6xl font-bold">
+            Your one link for everything
+          </h1>
+          <h2 className="text-gray-500 text-xl mt-6">
+            Share your links, social profiles,
+            contact info in more on one page
+          </h2>
+        </div>
+        <form
+          className="inline-flex items-center shadow-lg shadow-gray-500/20"
+        >
+          <span className="bg-white py-4 px-4">
+            linklist.to
+          </span>
+          <input
+            type='text'
+            className="py-4"
+            placeholder="username"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-4 px-6"
+          >
+            join for free
+          </button>
+        </form>
+      </section>
+
     </main>
   )
 }
